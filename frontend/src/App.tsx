c40e207f19b47c6024232e6277e630304ef00f3c
Michael Wang <a0416517630@icloud.com>
2025-09-02 11:03:49 +1000

    fixed routes and removed provider in main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
 
     // create the socket once on mount
     // deployed backend server: https://code-canvas-backend-r1z9.onrender.com
-    const server = "http://localhost:5000";
+    const server = "https://localhost:5000";
     const connectionOptions = {
       "force new connection": true,
       reconnectionAttempts: Infinity,
@@ -84,6 +84,7 @@ function App() {
                 <CodeCanvas users={users} user={user!} socket={socket} />
               </AuthenticatedRoute>
             } />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </BrowserRouter>
         <Toaster />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,13 @@ import App from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
 import { ColorModeProvider } from "@/components/ui/color-mode"
 import { Theme } from "@chakra-ui/react"
-import { AuthProvider } from './components/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <ColorModeProvider forcedTheme="dark">
         <Theme appearance="dark">
-          <AuthProvider>
             <App />
-          </AuthProvider>
         </Theme>
       </ColorModeProvider>
     </Provider>
